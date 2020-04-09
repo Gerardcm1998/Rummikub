2fe872aac54d94daf493c385898e730e6d403d3a
Gerardcm1998 <55488772+Gerardcm1998@users.noreply.github.com>
2020-04-09 20:23:19 +0200

    feta la funcio de canvi de torn

---
+++ resources/js/game.js
@@ -0,0 +1,30 @@
+
+function stealToken(actualPlayerId) {
+    giveToken();
+    changeTurn(actualPlayerId);
+}
+
+function giveToken() {
+    //TODO
+}
+
+function changeTurn (actualPlayerId) {
+    var actualPlayer = parseInt(actualPlayerId.charAt(actualPlayerId.length-1));
+    let numberOfPlayers = parseInt($("#numberOfPlayers").val());
+    finishTurn(actualPlayer);
+    if (actualPlayer >= numberOfPlayers) {
+        startTurn(1);
+    } else {
+        startTurn(actualPlayer+1);
+    }
+}
+
+function finishTurn(player) {
+    $("#player"+player).removeClass("playerSelected");
+    $("#player"+player).addClass("playerNotSelected");
+}
+
+function startTurn(player) {
+    $("#player"+player).removeClass("playerNotSelected");
+    $("#player"+player).addClass("playerSelected");
+}
